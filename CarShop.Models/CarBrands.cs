e6c6f0e0438ead3bd975ed58b2333c92d98e3109
V1zz <chumachenkoilya@gmail.com>
2017-03-06 21:23:04 +0200

    Add project files.

---
+++ CarShop.Models/CarBrands.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarShop.Models
+{
+    public class CarBrands
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CarBrands()
+        {
+            CarModels = new HashSet<CarModels>();
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Brand { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CarModels> CarModels { get; set; }
+    }
+}
---
+++ EFExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿using EFExamples;
+using System;
+
+namespace EFExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new CarShopDataModel())
+            {
+                context.SaveChanges();
+                foreach (var car in context.Cars)
+                {
+                    
+                    var carFullName = $"{car.CarModels.CarBrands.Brand} {car.CarModels.Model} {car.Year.Year}";
+                    Console.WriteLine(carFullName);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
